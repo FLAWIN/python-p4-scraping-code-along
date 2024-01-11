e057b40a4311f939b32e14c6f93c01e76ce105f8
Aloe <aloeflawingulenywa98@gmail.com>
2024-01-11 11:06:50 +0300

    Done

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -1,8 +1,16 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 import requests
-from Course import Course
 import ipdb
+import Course
+import json
 
 
 class Scraper:
-    pass
+    def __init__(self):
+        self.courses = []
+    def get_page():
+        # more code coming soon!
+        doc =  BeautifulSoup(requests.get("http://learn-co-curriculum.github.io/site-for-scraping/courses").text, 'html.parser')
+        ipdb.set_trace()
+
+Scraper.get_page()
